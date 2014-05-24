701646fbe45639bd5cfd72a149636d170ffcb072
Nishant Shukla <nick722@gmail.com>
2014-05-24 01:42:58 -0400

    removed a background gif

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 console.log('\'Allo \'Allo!');
 
 $(function() {
-   var n = Math.floor((Math.random() * 9) + 1);
+   var n = Math.floor((Math.random() * 8) + 1);
    $('body').css("background", 
                  "rgba(0, 0, 0, 0) url(/images/bg0"+ n +".gif) no-repeat fixed 50% 50% / cover padding-box border-box")
 });
